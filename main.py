037fb7ac54fa2933af150028e45fb1d3e793d29a
Emir Elçuk <123772472+emirelcuk@users.noreply.github.com>
2024-07-10 09:12:46 +0300

    Multiplication has been added to the calculator. It can currently perform 3 operations

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ def toplama(a, b):
 def cikarma(a, b):
     return a - b
 
+def carpma(a, b):
+    return a * b
+
 print("Yapılacak işlemi seçin:")
 print("1. Toplama")
 print("2. Çıkarma")
+print("3. Çarpma")
 
-secim = input("Seçiminizi yapın (1 veya 2): ")
+secim = input("Seçiminizi yapın (1, 2 veya 3): ")
 
 sayi1 = float(input("Birinci sayıyı girin: "))
 sayi2 = float(input("İkinci sayıyı girin: "))
@@ -19,5 +23,8 @@ if secim == "1":
 elif secim == "2":
     sonuc = cikarma(sayi1, sayi2)
     print(f"{sayi1} - {sayi2} = {sonuc}")
+elif secim == "3":
+    sonuc = carpma(sayi1, sayi2)
+    print(f"{sayi1} * {sayi2} = {sonuc}")
 else:
-    print("Geçersiz seçim. Lütfen 1 veya 2 girin.")
+    print("Geçersiz seçim. Lütfen 1, 2 veya 3 girin.")
