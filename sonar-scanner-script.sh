8c7f24962872b2737ca8a12630ee605f3fab0489
Betül Çetin <116976325+betul-cetin@users.noreply.github.com>
2024-07-19 10:02:16 +0300

    Update sonar-scanner-script.sh

--- sonar-scanner-script.sh
+++ sonar-scanner-script.sh
@@ -5,15 +5,14 @@ set -e
 
 # Variables
 SONAR_SCANNER_HOME='./sonar-scanner'
+SONAR_HOST_URL='http://dead-84-51-5-150.ngrok-free.app' # Your ngrok URL
 
 # Navigate to the directory to be scanned
 cd $GITHUB_WORKSPACE
 
 # Run SonarScanner
 $SONAR_SCANNER_HOME/bin/sonar-scanner \
-  -Dsonar.projectKey=my_project \
+  -Dsonar.projectKey=your_project_key \
   -Dsonar.sources=. \
-  -Dsonar.host.url= https://dead-84-51-5-150.ngrok-free.app
-  -Dsonar.login=squ_e09d97410282ff1551ca4f62de1f40117daf87bd
-
-# Process results as needed
+  -Dsonar.host.url=$SONAR_HOST_URL \
+  -Dsonar.login=$SONAR_TOKEN
